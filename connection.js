a1ebeba2335ac212ae079429f4bb7831a32578e9
Amit Gupta <amitg.inbox@gmail.com>
2022-07-18 16:05:19 +0100

    connection changes

--- connection.js
+++ connection.js
@@ -2,10 +2,7 @@ import { Kafka } from "kafkajs";
 import fs from "fs";
 
 // replace the value with your kafka service URI
-// const AIVEN_KAFKA_SERVICE_URI = "YOUR KAFKA SERVICE URI";
-
-const AIVEN_KAFKA_SERVICE_URI =
-  "kafka-nodejs-amitgup-6336.aivencloud.com:12424";
+const AIVEN_KAFKA_SERVICE_URI = "YOUR KAFKA SERVICE URI";
 
 // Configure kafkajs for connection to Aiven Kafka
 export const kafka = new Kafka({
